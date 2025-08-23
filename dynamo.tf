33fb5dab4f18e4450f7bd7f05ac0dc98af95a884
CarvalhoCortes <carvalhocortes@gmail.com>
2025-08-23 19:50:29 -0300

    Add .env file for AWS configuration and format hash_key in DynamoDB resource

--- dynamo.tf
+++ dynamo.tf
@@ -5,7 +5,7 @@ resource "aws_dynamodb_table" "files" {
   read_capacity  = 5
   write_capacity = 5
 
-  hash_key = "email"
+  hash_key  = "email"
   range_key = "fileName"
   attribute {
     name = "email"
