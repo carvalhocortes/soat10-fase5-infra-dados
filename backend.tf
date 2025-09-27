00fda423aff5eddebdd58d5c7d6df9809250edbf
Samuel Victor <samuel@samuelvictor.dev>
2025-09-27 13:03:47 -0300

    add GSIs

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket = "postech-soat10-bucket-fase5"
     key    = "github-actions-fiap/infra-dados/terraform.tfstate"
 
-    region  = "us-west-2"
+    region  = var.region
     encrypt = true
   }
 
